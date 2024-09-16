41704e34119db24111f595167e47f8f297039f52
Cameron Smith <Cs11@usf.edu>
2024-09-15 22:49:10 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,11 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+let inventory = [
+    { name: 'Refridgerator', price: 1800, quantity: 8, lowStockLevel 2 },
+    { name: 'Microwave', price: 600, quantity: 7, lowStockLevel: 4},
+    { name: 'Washing Machine', price: 2000, quantity: 1, lowStockLevel: 8},
+    { name: 'Televison', price: 400, quantity: 5, lowStockLevel: 3},
+    { name: 'Drying Machine', price: 700, quantity: 6, lowStockLevel: 1},
+
+
+];
